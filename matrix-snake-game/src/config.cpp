a0ec0a3ed15158077ce54cb06ba643b9c4902b46
Tóth Nikolett <toth.nikolett@quanopt.com>
2019-09-11 16:48:34 +0200

    Revert "Added readme"
    This reverts commit 487e0cbcf392c59b35d8e03510c7971764760f3a.

--- matrix-snake-game/src/config.cpp
+++ matrix-snake-game/src/config.cpp
@@ -3,7 +3,7 @@
 using namespace config;
 
 const tick config::game::query = 800;
-const int config::game::maxLength = 5;
+const int config::game::maxLength = 40;
 const QUrl config::game::url("https://matrix-snake.sch.bme.hu/status");
 
 int config::dimension::width = 32;
